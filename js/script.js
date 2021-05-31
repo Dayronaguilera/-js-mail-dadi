d22e10cc7b4322b9c2d26bfe6e6c02dcbf732343
Dayronaguilera <aguilera.dayron@gmail.com>
2021-05-31 17:40:44 +0200

    End esercizio 2

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
 
 var email = prompt("inserisci la tua email");
 var vettoreEmail = ["mariorossi@gmail.com" , "pincopallo@gmail.com", "aguilera.dayron@gmail.com"];
-var chek = false;
+var check = false;
 
 for (var i = 0; i < vettoreEmail.length; i++) {
     if (vettoreEmail[i] === email) {
-        chek = true;
+        check = true;
         break;  
     }    
 }
-if (chek) {
+if (check) {
     document.getElementById("login").innerHTML = "login effettuato"
     
 } else {
     document.getElementById("login").innerHTML = "login non effettuato email errata"
 }
+
+// gioco dei dadi ------------------
+var dadoPc = Math.random(1,5)
+var dadoUtente = Math.random(1,5)
+
+if (dadoUtente > dadoPc) {
+    alert("HAI VINTOOOOOOOOOOO")
+} else {
+    alert("hai perso")
+}
+
